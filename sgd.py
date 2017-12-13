19588ecc5af353f26b4e992372607df7bb940e8e
itaisoudry <itai@availo.me>
2017-12-13 20:40:44 +0200

    working on sgd a

--- sgd.py
+++ sgd.py
@@ -7,6 +7,7 @@ import numpy.random
 import sklearn.preprocessing
 from numpy import *
 from sklearn.datasets import fetch_mldata
+from sklearn.cross_validation import cross_val_score
 
 mnist = fetch_mldata('MNIST original', data_home='./')
 data = mnist['data']
@@ -31,7 +32,7 @@ validation_data = sklearn.preprocessing.scale(validation_data_unscaled, axis=0, 
 test_data = sklearn.preprocessing.scale(test_data_unscaled, axis=0, with_std=False)
 
 
-def svm(samples, labels, C, n0, T):
+def sgd(samples, labels, C, n0, T):
     w = numpy.zeros(784)
 
     for t in range(T):
@@ -41,9 +42,34 @@ def svm(samples, labels, C, n0, T):
         sample = samples[i]
         nt = n0 / t
         sign = np.sign(label, np.dot(w, sample))
+        errors = 0
 
         if sign < 1:
             #  (1 − ηt)wt + ηtCyixi
             w = np.multiply((1 - nt), w) + np.multiply(nt * C * sign, sample)
         else:
             w = np.multiply((1 - nt), w)
+
+        if sign != label:
+            errors += 1
+    return w, errors
+
+
+def a():
+    avg_accuracies = []
+    for n in range(-5, 5):
+        n0 = pow(10, n)
+        # train svm
+        classifier, errors = sgd(train_data, train_labels, 1, n0, 1000)
+
+        accuracy = 0
+        # cross validation
+        for i in range(10):
+            accuracy += cross_val_score(classifier, validation_data, validation_labels, cv=5)
+
+        avg_accuracies.append(accuracy / 10)
+
+    # plot
+
+
+a()
